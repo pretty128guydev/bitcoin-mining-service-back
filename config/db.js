315001b48fa2809b98fb63ca078820477ed69055
pretty128guydev <pretty128guydev@outlook.com>
2024-09-18 14:08:50 +0100

    add database connection data

--- config/db.js
+++ config/db.js
@@ -2,6 +2,12 @@ const mysql = require("mysql2");
 const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "../.env") });
 
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSKEY,
+});
 db.connect((err) => {
   if (err) throw err;
   console.log("Connected to MySQL");
@@ -12,6 +18,13 @@ db.connect((err) => {
       console.log("Database created");
     }
   );
+  const db2 = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSKEY,
+    database: process.env.DB_NAME,
+  });
   db2.query(
     `CREATE TABLE IF NOT EXISTS users ( 
     id INT AUTO_INCREMENT PRIMARY KEY,
